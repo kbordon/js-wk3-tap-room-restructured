a95fd508d107b1dd256e09f3735aa53fc3a185bc
Kimberly Bordon and Valia Dempsey <kimberly+valia@>
2017-12-06 16:23:34 -0800

    add method to change employee property

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 | As a patron, a user can see list of all available kegs. | User clicks menu. | Page displays: <br><br> Keg name <br> brand <br> price<br> alcoholContent <br>beer style <br> Amount of pints in keg|
 | As employee, user can add a new keg with a form | User clicks "Add Keg." |   Page displays form with above properties. |
 | As employee, user can edit a keg's properties after entering them. | User clicks "Edit" | Page displays edit form. |
-| User can see how many pints are in a keg. | User looks at page | Page displays progress bar on keg. |
+| *User can see how many pints are in a keg. (based on absolute position, have a glass or some container represent the level of pints)| User looks at page | Page displays progress bar on keg. |
 | As employee, user can click a button next to keg to sell a pint of it. | User clicks sell.| Page updates keg amount. |
 | *As employee, user can select a radio button before clicking sell to change the amount of pints sold. | User selects radio button, and clicks sell. | Page updates keg amount. |
-| As employee, User can see kegs with less than 10 pints left. | User looks at page. | Page appends asterisk to low kegs. |
+| As employee, User can see kegs with less than 20 pints left. | User looks at page. | Page turns background color of low kegs to red. |
 | As patron, User can identify keg prices by color codes. | User looks at page. | Page displays color coded prices. |
 | As a patron, User identifies beers with content stronger than 7% with a skull and crossbones emoji. | User looks at page. | Page appends skull and crossbones to appropriate kegs. |
--- src/app.component.ts
+++ src/app.component.ts
@@ -3,53 +3,131 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <div *ngFor="let currentKeg of allKegs">
+  <button (click)="changeUser()">Change User</button>
+  <div [class]="levelColor(currentKeg)" *ngFor="let currentKeg of allKegs">
     <h1>{{currentKeg.name}}</h1>
     <h2>{{currentKeg.brand}}</h2>
-    <p>{{currentKeg.price}}</p>
-    <p>{{currentKeg.aContent}}</p>
+    <p [class] = "priceColor(currentKeg)">{{currentKeg.price}}</p>
+    <p>{{currentKeg.aContent}}<span *ngIf="currentKeg.aContent > 6">💀</span></p>
     <p>{{currentKeg.style}}</p>
     <p>{{currentKeg.pintsLevel}}</p>
+    <br>
+    <div *ngIf="tapRoomEmployee === true">
+      <button (click)="editKeg(currentKeg)">Edit</button>
+      <br>
+      <br>
+      <input type="radio" [(ngModel)]="beerSize" [value]="1">Pint<br>
+      <input type="radio" [(ngModel)]="beerSize" [value]="2">Growler<br>
+      <input type="radio" [(ngModel)]="beerSize" [value]="4">Large Growler<br>
+      <button *ngIf="currentKeg.pintsLevel > 16" (click)="sellPint(currentKeg, beerSize)">Sell</button>
+      <br>
+      <br>
+      <button (click)="removeKeg(currentKeg)">Remove</button>
+      <h2 *ngIf="currentKeg.pintsLevel === 12">Refill me please!</h2>
+    </div>
   </div>
 
-  <div class="new-form">
+  <div class="new-form" *ngIf="tapRoomEmployee === true">
     <h3>Add Keg</h3>
     <label>Name</label>
-    <br>
     <input type="text" [(ngModel)]="kegName">
-    <br>
+    <br><label>Brand</label>
     <input type="text" [(ngModel)]="kegBrand">
-    <br>
+    <br><label>Price</label>
     <input type="text" [(ngModel)]="kegPrice">
-    <br>
+    <br><label>Alcohol Content</label>
     <input type="text" [(ngModel)]="kegAContent">
-    <br>
+    <br><label>Beer Style</label>
     <input type="text" [(ngModel)]="kegStyle">
     <br>
     <button (click)="addKeg(kegName,kegBrand,kegPrice,kegAContent,kegStyle)">Add</button>
   </div>
 
-
-
+  <div *ngIf="selectedKeg">
+    <h3>Edit : name</h3>
+    <input [(ngModel)]="selectedKeg.name">
+    <br>
+    <input [(ngModel)]="selectedKeg.brand">
+    <br>
+    <input [(ngModel)]="selectedKeg.price">
+    <br>
+    <input [(ngModel)]="selectedKeg.aContent">
+    <br>
+    <input [(ngModel)]="selectedKeg.style">
+    <br>
+    <button (click)="finishEdit()">Done</button>
+  </div>
   `
 })
 
 export class AppComponent {
+  tapRoomEmployee: boolean = false;
   testKeg: Keg = new Keg("Half Hitch","Crux Fermentation Project",17,9.5,"Imperial IPA");
   allKegs: Keg[] = [this.testKeg];
+  selectedKeg = null;
 
+  changeUser(){
+    if (this.tapRoomEmployee === false){
+      this.tapRoomEmployee = true;
+      console.log(this.tapRoomEmployee);
+    } else {
+      this.tapRoomEmployee = false;
+    }
+  }
 
-  // displayTwo(testKeg){
-  //   return `${testKeg.name} ${testKeg.price}`;
-  // }
-  // Component logic
   addKeg(name, brand, price, aContent, style){
     this.allKegs.push(new Keg(name, brand, price, aContent, style));
   }
 
+  editKeg(clickedKeg){
+    this.selectedKeg = clickedKeg;
+  }
+
+  finishEdit(){
+    this.selectedKeg = null;
+  }
+
+  removeKeg(clickedKeg){
+    let kegIndex = this.allKegs.indexOf(clickedKeg);
+    if (kegIndex > -1) {
+      this.allKegs.splice(kegIndex, 1); // 1 stands for number of elements to remove, not index value of an array
+    }
+  }
+
+  sellPint(clickedKeg,beerSize){
+    if (clickedKeg.changeLevel(beerSize) === false){
+      alert("You need to refill this.");
+      // change this because you might not necessarily need to refill so much as change the selling size.
+    }
+  }
+
+  levelColor(currentKeg){
+    if (currentKeg.pintsLevel < 20) {
+      return "low";
+    }
+  }
+
+  priceColor(currentKeg){
+    if (currentKeg.price > 10){
+      return "over-ten";
+    } else if (currentKeg.price <= 10 && currentKeg.price >= 6) {
+      return "six-to-ten";
+    } else {
+      return "under-six";
+    }
+  }
 }
 
 export class Keg {
   public pintsLevel: number = 124;
   constructor(public name:string, public brand:string, public price:number, public aContent:number, public style:string){}
+
+  // the argument for the parameter 'modifier' comes from beerSize
+  changeLevel(modifier){
+    if(this.pintsLevel - (16*modifier) > 0) {
+      this.pintsLevel -= (16*modifier);
+    } else {
+      return false;
+    }
+  }
 }
